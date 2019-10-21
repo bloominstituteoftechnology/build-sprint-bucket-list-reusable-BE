fd09b27b3b0d9f4770d28c42be8a1006b388e0b6
Jenn Soderborg <jsoderborg7@gmail.com>
2019-10-20 21:33:02 -0600

    pg set up in production

--- knexfile.js
+++ knexfile.js
@@ -21,36 +21,18 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     },
     pool: {
       min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      max: 10,
     }
   }
-
 };
