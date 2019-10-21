9fcc79e128becdbaa6bc82ef3e340a1d0834bf6d
Jenn Soderborg <jsoderborg7@gmail.com>
2019-10-21 15:43:59 -0600

    added seed files for all tables, get tables by user-id is not working right, need to fix it

---
+++ data/seeds/003-items.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('items').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('items').insert([
+        {item_name: "Tokyo, Japan", journal_entry:"Best city in the world!", photo: "https://images.unsplash.com/photo-1547448526-5e9d57fa28f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80", completed: true},
+        {item_name: "Deer Valley Seafood Buffet", completed: false},
+        {item_name: "Harry Potter Series", journal_entry: "My favorites!", photo: "https://images.unsplash.com/photo-1500697017927-23abd276362a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", completed: true}
+      ]);
+    });
+};
---
+++ data/seeds/005-bucket-items.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('bucket-items').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('bucket-items').insert([
+        {item_id: 1, bucket_id: 1},
+        {item_id: 2, bucket_id: 2},
+        {item_id: 3, bucket_id: 3}
+      ]);
+    });
+};
