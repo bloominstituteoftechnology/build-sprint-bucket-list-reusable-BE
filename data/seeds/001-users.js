4acc1b8e7579408ad651c50780302bb0a345a691
Jenn Soderborg <jsoderborg7@gmail.com>
2019-10-23 10:53:58 -0600

    still messing with seeds

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -2,10 +2,10 @@ const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').truncate()
+  return knex('users')
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert([
+      return knex('users').upsert([
         {username: 'dummy1', password: bcrypt.hashSync('dummy1'), email: 'dummy1@mail.com'},
         {username: 'dummy2', password: bcrypt.hashSync('dummy2'), email: 'dummy2@mail.com'}
       ]);
